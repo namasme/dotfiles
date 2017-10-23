0100ce752ce0b559b18be4611e1de4d304a1a1b9
Nacho Mas <nacho.cronos2@gmail.com>
2017-10-23 12:56:13 +0200

    Adds basic configuration for xmonad and xmobar

---
+++ xmonad/.xmonad/xmonad.hs
@@ -0,0 +1,25 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+
+main = do
+    xmproc <- spawnPipe "xmobar ~/.xmobarrc"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        , modMask = mod4Mask     -- Rebind Mod to the Windows key
+	, terminal = "urxvtc"
+        } `additionalKeys`
+        [ ((mod4Mask .|. shiftMask, xK_z), spawn "/bin/xscreensaver-command -lock")
+        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((0, xK_Print), spawn "scrot")
+	, ((mod4Mask, xK_p), spawn "dmenu_run -fn 'Droid Sans Mono-9'")
+        ]
