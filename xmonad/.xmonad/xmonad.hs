84856aedbfe5ba6988cdf495cda36b59571b336c
Nacho Mas <nacho.cronos2@gmail.com>
2017-10-23 20:38:20 +0200

    Updates xmonad configuration
    * Changes window title color from green to grey (nicer)
    * Fixes xscreensaver command
    * Adds support for XF86 audio keys

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,25 +1,30 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.EZConfig(additionalKeys, additionalKeysP)
 import System.IO
 
 
 main = do
     xmproc <- spawnPipe "xmobar ~/.xmobarrc"
     xmonad $ defaultConfig
-        { manageHook = manageDocks <+> manageHook defaultConfig
+        { manageHook = manageDocks <+> (isFullscreen --> doFullFloat) <+> manageHook defaultConfig
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        , ppTitle = xmobarColor "grey" "" . shorten 50
                         }
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
 	, terminal = "urxvtc"
         } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "/bin/xscreensaver-command -lock")
+        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -activate")
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         , ((0, xK_Print), spawn "scrot")
 	, ((mod4Mask, xK_p), spawn "dmenu_run -fn 'Droid Sans Mono-9'")
+        ] `additionalKeysP`
+        [ ("<XF86AudioRaiseVolume>", spawn "amixer set Master 5%+ unmute")
+        , ("<XF86AudioLowerVolume>", spawn "amixer set Master 5%- unmute")
+        , ("<XF86AudioMute>"       , spawn "amixer set Master toggle")
         ]
