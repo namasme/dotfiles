44e5d04ecb4380d0c4a82ac1dd88fa420d55afe4
Nacho Mas <nacho.cronos2@gmail.com>
2017-10-23 12:45:14 +0200

    _Personal_ init.el
    * Binds .js and .jsx files to their respective modes
    * Sets up recentf
    * Enables C-x C-n

---
+++ emacs/.emacs.d/personal/init.el
@@ -0,0 +1,18 @@
+;; auto-mode-alist
+
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . rjsx-mode))
+
+;; Recent files configuration
+
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(global-set-key (kbd "C-x C-r") 'recentf-open-files)
+
+(run-at-time nil (* 60 60) 'recentf-save-list)
+
+;; enable C-x C-n
+
+(put 'set-goal-column 'disabled nil)
+
+
