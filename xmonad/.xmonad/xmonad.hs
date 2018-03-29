3b5b3838a9551d31148bc169fc3c16cc2fdcf638
Nacho Mas <nacho.cronos2@gmail.com>
2018-03-29 18:24:08 +0200

    Fixes (partially) urxvt appearance

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -9,20 +9,20 @@ import System.IO
 
 main = do
     xmproc <- spawnPipe "xmobar ~/.xmobarrc"
-    xmonad $ defaultConfig
-        { manageHook = manageDocks <+> (isFullscreen --> doFullFloat) <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+    xmonad $ docks $ def
+        { manageHook = manageDocks <+> manageHook def
+        , layoutHook = avoidStruts  $  layoutHook def
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "grey" "" . shorten 50
                         }
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
-	, terminal = "urxvtc"
+        , terminal = "urxvtc"
         } `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -activate")
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s ~/Pictures/screenshots/%Y-%m-%d_%T.png")
         , ((0, xK_Print), spawn "scrot ~/Pictures/screenshots/%Y-%m-%d_%T.png")
-	, ((mod4Mask, xK_p), spawn "dmenu_run -fn 'Droid Sans Mono-9'")
+        , ((mod4Mask, xK_p), spawn "dmenu_run -fn 'Droid Sans Mono-9'")
         ] `additionalKeysP`
         [ ("<XF86AudioRaiseVolume>", spawn "pactl set-sink-mute 0 false; pactl set-sink-volume 0 +5%") -- TODO: this can go over 100% (bad quality)
         , ("<XF86AudioLowerVolume>", spawn "pactl set-sink-mute 0 false; pactl set-sink-volume 0 -5%")
