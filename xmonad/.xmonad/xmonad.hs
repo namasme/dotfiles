374e386c1ab3514443ed9c690569237aa22cdba2
Nacho Mas <nacho.cronos2@gmail.com>
2018-08-28 16:21:05 +0200

    Updates xmonad.hs
    * Integrates it with other tools through the EWMH desktop standard (namely rofi)
    * xmobar no longer needs to be told the location of its config file (plus, it's been moved)
    * Mod+p now spawns rofi's dmenu version instead of plain dmenu
    * Mod+s shows rofi's windowswitcher
    * XF86 keys now use custom `volume` utility instead of raw pactl

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,5 +1,6 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Layout.NoBorders
@@ -10,8 +11,8 @@ import System.IO
 
 
 main = do
-    xmproc <- spawnPipe "xmobar ~/.xmobarrc"
-    xmonad $ defaultConfig
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ ewmh defaultConfig
         { manageHook = manageDocks <+> (isFullscreen --> doFullFloat) <+> manageHook defaultConfig
         , layoutHook = noBorders $ avoidStruts $ smartSpacing 5 $ layoutHook defaultConfig
         , logHook = dynamicLogWithPP xmobarPP
@@ -24,9 +25,10 @@ main = do
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -activate")
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s ~/Pictures/screenshots/%Y-%m-%d_%T.png")
         , ((0, xK_Print), spawn "scrot ~/Pictures/screenshots/%Y-%m-%d_%T.png")
-	, ((mod4Mask, xK_p), spawn "dmenu_run -fn 'Droid Sans Mono-9'")
+	, ((mod4Mask, xK_p), spawn "rofi -show run -location 1 -lines 3 -width 100")
+        , ((mod4Mask, xK_s), spawn "rofi -show windowcd")
         ] `additionalKeysP`
-        [ ("<XF86AudioRaiseVolume>", spawn "pactl set-sink-mute 0 false; pactl set-sink-volume 0 +5%") -- TODO: this can go over 100% (bad quality)
-        , ("<XF86AudioLowerVolume>", spawn "pactl set-sink-mute 0 false; pactl set-sink-volume 0 -5%")
-        , ("<XF86AudioMute>"       , spawn "pactl set-sink-mute 0 toggle")
+        [ ("<XF86AudioRaiseVolume>", spawn "volume --up")
+        , ("<XF86AudioLowerVolume>", spawn "volume --down")
+        , ("<XF86AudioMute>"       , spawn "volume --toggle-mute")
         ]
